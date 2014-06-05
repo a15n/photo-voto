2db6b18d1bd6a1c861c3e6e15e63dd36581f7474
Andrew Scheuermann <andrewscheuermann@gmail.com>
2014-06-05 17:32:19 -0400

    updated with instagram API

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,34 +2,21 @@
 
 angular.module('photoVotoApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-    // $http.get('https://api.instagram.com/v1/tags/cats/media/recent?callback=?&amp;client_id=a91636c3098f409d8c8c55a2bd255a32')
-    window.callbackFunction = function () {
-      $scope.getRequest = 'WILLIAM';
+    var getUrl = function (tag) {
+      return 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?callback=?&amp;client_id=a91636c3098f409d8c8c55a2bd255a32&callback=JSON_CALLBACK';
     };
-    var s = document.createElement('script');
-    s.src = 'https://api.instagram.com/v1/media/popular?client_id=a91636c3098f409d8c8c55a2bd255a32&callback=callbackFunction';
-    document.head.appendChild(s);
-
-    // $http.get('https://api.instagram.com/v1/media/popular?client_id=a91636c3098f409d8c8c55a2bd255a32&callback=callbackFunction');
-    // .success(function(data, status, headers, config) {
-    //   $scope.getRequest = 'success';
-    // })
-    // .error( function () {
-    //   $scope.getRequest = 'failure';
-    // });
+    // $http.jsonp('https://api.instagram.com/v1/media/popular?client_id=a91636c3098f409d8c8c55a2bd255a32&callback=JSON_CALLBACK')
+    $http.jsonp(getUrl('GoldenGate'))
+    .success(function(data) {
+      $scope.photos = data.data;
+      console.log(data);
+    })
+    .error( function () {
+    });
   });
 
+//comment
 
-/*
-know this is something specific to Angularjs. This works fine on my catgram app.
-
-http://stackoverflow.com/questions/21102690/angularjs-not-detecting-access-control-allow-origin-header
-http://stackoverflow.com/questions/3102819/disable-same-origin-policy-in-chrome
-
-angular JSONP handling
-https://docs.angularjs.org/api/ng/service/$http
-
-*/
+//attach jquery event to window.resize
+//SO dynamically resize div based on size of browser window
+//emilolsson.com/shop/demo/l
